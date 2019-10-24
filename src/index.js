7d959b048c7c71df733c654a0983caff3f18f84f
Egor Shkirya <zuhijan@yandex.ru>
2019-10-24 22:26:16 +0600

    webpack, babel, postcss, configs, src, hash

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import "./pages/add-resume/blocks/add-resume-page.css";
+import "./pages/add-vacancy/blocks/add-vacancy-page.css";
+import "./pages/default/blocks/default-page.css";
+import "./pages/profile-org/blocks/profile-org-page.css";
+import "./pages/vacancy/blocks/vacancy-page.css";
